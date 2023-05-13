240252f01dbef391a537fb9984ecde882a80ee46
Emirhan <emirhanduman36@hotmail.com>
2023-05-14 02:50:36 +0300

    SignUpActivity created and most of it completed.

--- app/src/main/java/com/emirhanduman/coinmarkt/MainActivity.kt
+++ app/src/main/java/com/emirhanduman/coinmarkt/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.emirhanduman.coinmarkt
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //There won't be action bar in main activity
+        supportActionBar?.hide()
+
+        //2 seconds delay
+        Handler(Looper.getMainLooper()).postDelayed({
+            //start next activity
+            val intent = Intent(this,SignUpActivity::class.java)
+            startActivity(intent)
+
+            //finish this activity
+            finish()
+        },5000)
+
     }
 }
