a8e87b11d0c6c39270a94856f22dec019bf3eec0
Emirhan <emirhanduman36@hotmail.com>
2023-06-06 15:21:34 +0300

    fragment_home.xml is edited. RecyclerView is removed. Also, made some design changed. Retrofit is implemented to the project to turned API interfaces into callable objects.

--- app/src/main/java/com/emirhanduman/coinmarkt/apis/ApiInterface.kt
+++ app/src/main/java/com/emirhanduman/coinmarkt/apis/ApiInterface.kt
@@ -1,4 +1,13 @@
 package com.emirhanduman.coinmarkt.apis
 
+import com.emirhanduman.coinmarkt.models.MarketModel
+import retrofit2.Response
+import retrofit2.http.GET
+
 interface ApiInterface {
+
+    //"suspend fun" a function that can be paused and resumed at a later time.
+    //"Coroutines basics" read the documentation in detail laterw
+    @GET("data-api/v3/cryptocurrency/listing?start=1&limit=500")
+    suspend fun getMarketData() : Response<MarketModel>
 }
--- app/src/main/java/com/emirhanduman/coinmarkt/apis/ApiUtilities.kt
+++ app/src/main/java/com/emirhanduman/coinmarkt/apis/ApiUtilities.kt
@@ -1,4 +1,14 @@
 package com.emirhanduman.coinmarkt.apis
 
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 object ApiUtilities {
+
+    fun getInstance() : Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://api.coinmarketcap.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
 }
